853c4b4a5a6d08db540fefea152a6dce425e84ef
KertesVi <kerteszviki@gmail.com>
2025-02-05 14:39:31 +0100

    add env front_URL

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors({ origin: "mops-hotel.vercel.app", // Allow only your frontend
+app.use(cors({ origin: process.env.FRONT_URL, // Allow only your frontend
   methods: [ "POST"], // Allowed HTTP methods
   credentials: true, // Allow cookies and authentication 
 }));
