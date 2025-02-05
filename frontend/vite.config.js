ae4d3fde9df9a9dce6138b7d3646af89611a4778
KertesVi <kerteszviki@gmail.com>
2025-02-05 11:02:06 +0100

    vit config update with port

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://0.0.0.0:10000',
         changeOrigin: true,
         secure: false,
       },
