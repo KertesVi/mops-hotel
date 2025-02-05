d205741ad4c80ea82b9541d21e932d409359e65b
KertesVi <kerteszviki@gmail.com>
2025-02-05 11:50:48 +0100

    Fix port binding for frontend and backend

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-const port = process.env.PORT;
+const port = process.env.PORT || 10000;
 const mongoURI = process.env.MONGO_URI;
 
 
