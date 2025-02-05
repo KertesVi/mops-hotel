af1b8f77f3792b66bd61d4939c10a2ede6c2b04c
KertesVi <kerteszviki@gmail.com>
2025-02-05 10:42:46 +0100

    fixing port issue via vite conf

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-const port = process.env.PORT || 10000;
+const port = process.env.PORT;
 const mongoURI = process.env.MONGO_URI;
 
 
