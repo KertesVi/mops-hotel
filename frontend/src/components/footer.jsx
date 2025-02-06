9359b0b49d1329ac9444ce50538b8f6c208880df
KertesVi <kerteszviki@gmail.com>
2025-02-06 11:09:33 +0100

    create responsive footer and navBar

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -1,4 +1,22 @@
+import { useState, useEffect } from "react";
+
 function Footer() {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+  useEffect(() => {
+    // Function to update state based on window size
+    const handleResize = () => setIsMobile(window.innerWidth < 768);
+
+    // Attach event listener
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup listener on unmount
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  // Hide footer on mobile screens
+  if (isMobile) return null;
+  
   return (
     <>
       <footer className="w3-padding-12 w3-black w3-bottom w3-center w3-margin-top">
@@ -7,6 +25,7 @@ function Footer() {
           <a
             href="https://devopsjourney.cloud"
             target="_blank"
+            rel="noopener noreferrer"
             className="w3-hover-text-green"
           >
             Kertész Viktória
--- frontend/src/components/navigationBar.jsx
+++ frontend/src/components/navigationBar.jsx
@@ -1,23 +1,70 @@
+import { useState } from "react";
 import { Link } from 'react-router-dom';
 
 
 function NavigationBar() {
-    return (
-      <>
-        <div className="w3-top w3-large">
-          <div className="w3-bar w3-white w3-card" id="myNavbar">
-            <Link to="/" className="w3-bar-item w3-button w3-wide">Mops Hotel Pilisvörösvár</Link>
-        
-            <div className="w3-right w3-hide-small">
-              <Link to="/aboutUs" className="w3-bar-item w3-button"><i className="fa fa-user"></i> Bemutatkozás</Link>
-              <Link to="/booking" className="w3-bar-item w3-button"><i className="fa fa-th"></i> Foglalás</Link>
-              <Link to="/contact" className="w3-bar-item w3-button"><i className="fa fa-envelope"></i> Kapcsolat</Link>
-            </div>
+  const [menuOpen, setMenuOpen] = useState(false);
+  return (
+    <>
+      <div className="w3-top w3-large">
+        <div className="w3-bar w3-white w3-card" id="myNavbar">
+          <Link to="/" className="w3-bar-item w3-button w3-wide">
+            Mops Hotel Pilisvörösvár
+          </Link>
+
+          {/* Hamburger Icon for Mobile */}
+          <button
+            className="w3-bar-item w3-button w3-right w3-hide-large w3-hide-medium"
+            onClick={() => setMenuOpen(!menuOpen)}
+          >
+            ☰
+          </button>
+
+          {/* Desktop Menu */}
+          <div className="w3-right w3-hide-small">
+            <Link to="/aboutUs" className="w3-bar-item w3-button">
+              <i className="fa fa-user"></i> Bemutatkozás
+            </Link>
+            <Link to="/booking" className="w3-bar-item w3-button">
+              <i className="fa fa-th"></i> Foglalás
+            </Link>
+            <Link to="/contact" className="w3-bar-item w3-button">
+              <i className="fa fa-envelope"></i> Kapcsolat
+            </Link>
           </div>
         </div>
-      </>
-    );
-  }
+
+        {/* Mobile Dropdown Menu */}
+        {menuOpen && (
+          <div className="w3-bar-block w3-white w3-hide-large w3-hide-medium">
+            <Link
+              to="/aboutUs"
+              className="w3-bar-item w3-button"
+              onClick={() => setMenuOpen(false)}
+            >
+              <i className="fa fa-user"></i> Bemutatkozás
+            </Link>
+            <Link
+              to="/booking"
+              className="w3-bar-item w3-button"
+              onClick={() => setMenuOpen(false)}
+            >
+              <i className="fa fa-th"></i> Foglalás
+            </Link>
+            <Link
+              to="/contact"
+              className="w3-bar-item w3-button"
+              onClick={() => setMenuOpen(false)}
+            >
+              <i className="fa fa-envelope"></i> Kapcsolat
+            </Link>
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
+
   
   export default NavigationBar;
   
