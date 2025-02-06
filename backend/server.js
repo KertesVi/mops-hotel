2440f31130871840e17f690db28f0174ae13dedf
KertesVi <kerteszviki@gmail.com>
2025-02-06 17:58:40 +0100

    fixing cors error

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors({ origin: "https://www.mopshotel.fun" }));
+app.use(cors({ origin: ["https://www.mopshotel.fun", "https://mops-hotel.vercel.app"] }));
 
 
 const port = process.env.PORT || 4000;
